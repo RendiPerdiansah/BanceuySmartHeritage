890812d75eb93cc4eeb27bb50bcbf2ae7769ae75
Rens <rendipperdiansah16@gmail.com>
2025-06-11 11:12:23 +0700

    update css login

--- resources/views/kaulinan_barudak.blade.php
+++ resources/views/kaulinan_barudak.blade.php
@@ -51,72 +51,62 @@
         <div class="konten-header">Kaulinan Barudak</div>
 
         <div class="konten-container-content">
-            <!-- Celempung-->
-            <h2>Celempung</h2>
+            <!-- Gusur Upih-->
+            <h2>Gusur Upih</h2>
             <div class="konten-kaulinan-container">
                 <div class="konten-text-box">
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit excepturi saepe praesentium nesciunt eius deserunt cum ratione iste ullam illo neque quaerat, quidem maiores necessitatibus. Officia explicabo dolor dolorem quis!</p>
                 </div>
                 <div class="konten-image-box">
-                    <img src="{{ asset('assets/images/celempung.jpg') }}" alt="Celempung">
+                    <img src="" alt="Gusur Upih">
                 </div>
             </div>
     
-            <!-- Gemyung -->
-            <h2>Gemyung</h2>
+            <!-- Engkle -->
+            <h2>Engkle</h2>
             <div class="konten-kaulinan-container konten-reverse">
                 <div class="konten-text-box">
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda sequi aspernatur eius facilis a aliquam dignissimos sunt quam dolorum quia. Maiores, laboriosam optio aperiam repellat voluptas ut eaque quod quia.</p>
                 </div>
                 <div class="konten-image-box">
-                    <img src="{{ asset('assets/images/gemyung.jpg') }}" alt="Gemyung">
+                    <img src="" alt="Engkle">
                 </div>
             </div>
     
-            <!-- Dogdog -->
-            <h2>Dogdog</h2>
+            <!-- Jajangkungan -->
+            <h2>Jajangkungan</h2>
             <div class="konten-kaulinan-container">
                 <div class="konten-text-box">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quam minima perferendis suscipit sint reprehenderit veniam delectus eligendi tenetur! Dolorum veniam ad aspernatur quae ut cumque harum suscipit quibusdam maxime!</p>
                 </div>
                 <div class="konten-image-box">
-                    <img src="{{ asset('assets/images/dogdog.jpg') }}" alt="Dogdog">
+                    <img src="" alt="Jajangkungan">
                 </div>
             </div>
     
-            <!-- Rengkong -->
-            <h2>Rengkong</h2>
+            <!-- Sorodot Gaplok -->
+            <h2>Sorodot Gaplok</h2>
             <div class="konten-kaulinan-container konten-reverse">
                 <div class="konten-text-box">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem veritatis, atque sed sint quod quam temporibus. Molestias porro atque numquam provident possimus. Eaque modi vitae facilis eius, odio dicta officiis.</p>
                 </div>
                 <div class="konten-image-box">
-                    <img src="{{ asset('assets/images/rengkong.jpg') }}" alt="Rengkong">
+                    <img src="" alt="Sorodot Gaplok">
                 </div>
             </div>
     
-            <!-- Durkeung  -->
-            <h2>Durkeung </h2>
+            <!-- Gatrik  -->
+            <h2>Gatrik </h2>
             <div class="konten-kaulinan-container">
                 <div class="konten-text-box">
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis fugit doloribus, ullam illum dolores esse doloremque voluptatum, obcaecati temporibus et ducimus provident numquam omnis assumenda explicabo tempora. Eius, similique reprehenderit?</p>
                 </div>
                 <div class="konten-image-box">
-                    <img src="{{ asset('assets/images/durkeung.jpg') }}" alt="Durkeung">
+                    <img src="" alt="Gatrik">
                 </div>
             </div>
     
-            <!-- Tutunggulan -->
-            <h2>Tutunggulan</h2>
-            <div class="konten-kaulinan-container konten-reverse">
-                <div class="konten-text-box">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore vero eos, id, veniam nam odio eligendi debitis doloremque laboriosam accusamus incidunt dignissimos possimus natus, at deserunt hic. Enim, alias debitis!</p>
-                </div>
-                <div class="konten-image-box">
-                    <img src="{{ asset('assets/images/tutunggulan.jpg') }}" alt="Tutunggulan">
-                </div>
-            </div>
-    
+           
             
         </div>
     </div>
